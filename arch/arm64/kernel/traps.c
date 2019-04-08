fb86705515df4999f5b13f7112ca41f173509061
Will Deacon <will.deacon@arm.com>
2019-04-08 17:56:34 +0100

    arm64: backtrace: Don't bother trying to unwind the userspace stack
    commit 1e6f5440a6814d28c32d347f338bfef68bc3e69d upstream.
    
    Calling dump_backtrace() with a pt_regs argument corresponding to
    userspace doesn't make any sense and our unwinder will simply print
    "Call trace:" before unwinding the stack looking for user frames.
    
    Rather than go through this song and dance, just return early if we're
    passed a user register state.
    
    Cc: <stable@vger.kernel.org>
    Fixes: 1149aad10b1e ("arm64: Add dump_backtrace() in show_regs")
    Reported-by: Kefeng Wang <wangkefeng.wang@huawei.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/arm64/kernel/traps.c
+++ arch/arm64/kernel/traps.c
@@ -113,7 +113,7 @@ static void dump_instr(const char *lvl, struct pt_regs *regs)
 void dump_backtrace(struct pt_regs *regs, struct task_struct *tsk)
 {
 	struct stackframe frame;
-	int skip;
+	int skip = 0;
 
 #if (defined CONFIG_RKP_CFP_ROPP) && (defined CONFIG_RKP_CFP_TEST)
 	unsigned long value = 0x0;
@@ -123,6 +123,12 @@ void dump_backtrace(struct pt_regs *regs, struct task_struct *tsk)
 #endif
 
 	pr_debug("%s(regs = %p tsk = %p)\n", __func__, regs, tsk);
+
+	if (regs) {
+		if (user_mode(regs))
+			return;
+		skip = 1;
+	}
 
 	if (!tsk)
 		tsk = current;
@@ -149,7 +155,6 @@ void dump_backtrace(struct pt_regs *regs, struct task_struct *tsk)
 	frame.graph = tsk->curr_ret_stack;
 #endif
 
-	skip = !!regs;
 	printk("Call trace:\n");
 	do {
 		/* skip until specified stack frame */
@@ -213,10 +218,10 @@ static int __die(const char *str, int err, struct pt_regs *regs)
 		return ret;
 
 	print_modules();
-	__show_regs(regs);
 	pr_emerg("Process %.*s (pid: %d, stack limit = 0x%p)\n",
 		 TASK_COMM_LEN, tsk->comm, task_pid_nr(tsk),
 		 end_of_stack(tsk));
+	show_regs(regs);
 
 	if (!user_mode(regs)) {
 #if 0 //#ifdef CONFIG_SEC_DEBUG
@@ -229,7 +234,6 @@ static int __die(const char *str, int err, struct pt_regs *regs)
 					+ (unsigned long)task_stack_page(tsk));
 		}
 #endif
-		dump_backtrace(regs, tsk);
 		dump_instr(KERN_EMERG, regs);
 	}
 
