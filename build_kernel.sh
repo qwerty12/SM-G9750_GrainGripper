2b85823b42fdf5b57d5c022b7bee16b0fc3da884
Faheem Pervez <trippin1@gmail.com>
2019-08-16 00:50:30 +0100

    Run sensible number of parallel instances & fix CFP compiler path

--- build_kernel.sh
+++ build_kernel.sh
@@ -10,12 +10,14 @@ mkdir out
 
 BUILD_CROSS_COMPILE=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 KERNEL_LLVM_BIN=$(pwd)/toolchain/llvm-arm-toolchain-ship/6.0/bin/clang
+KERNEL_LLVM_CFP=$(pwd)/toolchain/llvm-arm-toolchain-ship/6.0-cfp/bin/clang
 #BUILD_CROSS_COMPILE=$BUILD_TOP_DIR/PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 #KERNEL_LLVM_BIN=$BUILD_TOP_DIR/toolchain/llvm-arm-toolchain-ship/6.0/bin/clang
 CLANG_TRIPLE=aarch64-linux-gnu-
 KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
+PROC="$(($(nproc) + 1))"
 
-make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE beyond2qlte_chn_open_defconfig
-make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE
+make -j$PROC -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CFP_CC=$KERNEL_LLVM_CFP CLANG_TRIPLE=$CLANG_TRIPLE beyond2qlte_chn_open_defconfig
+make -j$PROC -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CFP_CC=$KERNEL_LLVM_CFP CLANG_TRIPLE=$CLANG_TRIPLE
  
 cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
